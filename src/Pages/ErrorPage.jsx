c27b5e192aac28d57c152ce7f8196b545fda2913
MD TAMIUL ALAM <eishatahmedtamim70@gmail.com>
2025-10-10 12:59:14 +0600

    added error file

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+import errorImg from '../assets/error-404.png'
+const ErrorPage = () => {
+    return (
+        <div className='flex justify-center text-center my-[50px] '>
+            <div className='space-y-3'>
+                <img src={errorImg} alt="errorImg" />
+                <h1 className='font-bold text-4xl'>Oops, page not found!</h1>
+                <p>The page you are looking for is not available.</p>
+                <Link className="btn bg-[linear-gradient(125.07deg,rgba(99,46,227,1),rgba(159,98,242,1)_100%)] text-white border-none hover:opacity-90" to='/'>Go Back</Link>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
