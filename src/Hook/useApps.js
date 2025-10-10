ba58fa671d5f2676ccb7a56fe0826e94a957e2ae
MD TAMIUL ALAM <eishatahmedtamim70@gmail.com>
2025-10-10 12:57:45 +0600

    added custom hooks

--- src/Hook/useApps.js
+++ src/Hook/useApps.js
@@ -1,3 +1,23 @@
+import { useEffect, useState } from 'react'
+const useApps = () => {
+  const [apps, setapps] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    setLoading(true)
+    axios('/AppData.json')
+      .then(data => setapps(data.data))
+      .catch(err => setError(err))
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      });
+  }, []);
+  return { apps, loading, error }
+};
+
+export default useApps;
 
 
 
